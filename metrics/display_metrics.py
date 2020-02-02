3c15aba40f3a56c78c23626399346dbab0722778
Aleksandar Aleksandrov <aaleksandar.aleksandrov@gmail.com>
2020-02-02 12:13:59 +0100

    Added more metrics

--- metrics/display_metrics.py
+++ metrics/display_metrics.py
@@ -27,6 +27,20 @@ def display_metrics(path: str):
     plt.show()
 
 
+def display_both_metrics():
+    rec_times = load_metrics("recipe_recommendation.txt")
+    ing_times = load_metrics("ingredient_recognition.txt")
+    times_count = range(1, len(rec_times) + 1)
+
+    plt.plot(times_count, rec_times)
+    plt.plot(times_count, ing_times)
+    plt.legend(['Recipe Recommendation', 'Ingredient Recognition'])
+    plt.xlabel('Attempt')
+    plt.ylabel('Execution time in ms')
+    plt.show()
+
+
 display_metrics("recipe_recommendation.txt")
 display_metrics("ingredient_recognition.txt")
 
+display_both_metrics()
